b8cce19a9cf7fcc0b0502ccbb3758b53da260365
Rene Angeles <nottwo@Renes-MacBook-Pro.local>
2022-11-28 17:30:09 -0600

    Corrected black jack

--- Games/black_jack/blackjack.py
+++ Games/black_jack/blackjack.py
@@ -1,18 +1,18 @@
 import random
 class BJ(object):
     def __init__(self, n_decks = 4):
-        numbs = list(range(1,14))
+        numbs = list(range(1,11))
+        numbs.append('J')
+        numbs.append('Q')
+        numbs.append('K')
         self.deck  = []
         for i in range(n_decks):
             self.deck+= numbs
-
-
-        self.vals = {i: (i if i<=11 else 10) for i in  range(2,14) }
-        self.val1 = self.vals
-        self.val2 = self.vals
-        self.val1[1] = 1
-        self.val2[1] = 11
-
+        self.vals = {i: (i if i<11 else 10) for i in  range(2,14) }
+        self.vals[1] = 1
+        self.vals['J'] = 10
+        self.vals['Q'] = 10
+        self.vals['K'] = 10
         self.dealer = []
         self.player = []
 
@@ -20,15 +20,31 @@ class BJ(object):
         card =self.deck.pop()
         person +=[card]
 
-    def scores(self,deck):
-        t1 = sum([self.val1[i] for i in deck])
-        t2 = sum([self.val2[i] for i in deck])
-        if t1 > 21 and t2 > 21:
-            return min(t1,t2)
-        else: return max(t1,t2)
+    def scores(self, deck):
+        _deck = deck.copy()
+        scores = [sum([self.vals[i] for i in _deck])]
+        while 1 in _deck:
+            _deck.remove(1)
+            _deck.append(2)
+            _deck.append(9)
+            scores.append(sum([self.vals[i] for i in _deck]))
+        under_or_equal  = -1
+        above = -1
 
+        for i in scores:
+            if i <= 21:
+                under_or_equal = i
+        for i in scores:
+            if i > 21:
+                above = i
+                break
+        if under_or_equal == -1 and above != -1:
+            return above
+        elif under_or_equal != -1:
+            return under_or_equal
+        else:
+            print("Error tracking the score of {}".fortmat(deck))
 
-        return (t1,t2)
 
     def start_game(self):
         self.give_card(game.player)
@@ -38,14 +54,6 @@ class BJ(object):
 
     def ask_player(self):
         print('Stay or not')
-
-    def check(self,person):
-        if self.scores(person) > 21:
-            return False
-        elif self.scores(person) == 21:
-            print('Black Jack!')
-            return True
-        return False
 
     def play_game(self):
         print('Dealer shuffles')
@@ -60,8 +68,6 @@ class BJ(object):
         print('Your cards are:')
         print('Card hidden {} and card up {}'.format(self.player[0], self.player[-1]) )
 
-        if self.check(self.player):
-            return
 
         while True:
             print('Do you want another card (y/n)?')
@@ -71,18 +77,31 @@ class BJ(object):
             elif decision == 'y':
                 self.give_card(self.player)
                 print('Your deck is now {}'.format(self.player))
-            if self.check(self.player):
-                return
+            if self.scores(self.player)>=21:
+                break
 
-        print('House plays')
-        while self.scores(self.dealer) <17:
+        if self.scores(self.player) == 21:
+            print("Black Jack! You win!")
+            return
+
+        elif self.scores(self.player) > 21:
+            print('House wins')
+            return
+
+        print('Now house plays')
+        print("House deck is: {}".format(self.dealer))
+        while self.scores(self.dealer) <17 and self.scores(self.dealer) != 21:
             self.give_card(self.dealer)
+            print("House deck is: {}".format(self.dealer))
 
-
-        if self.scores(self.dealer) < self.scores(self.player) and self.scores(self.player) < 22:
-            print('you win')
-        elif self.scores(self.dealer) == self.scores(self.player) and self.scores(self.player) < 22:
-            print('draw')
+        if self.scores(self.dealer) == 21:
+            print("Black Jack! House wins!")
+        elif self.scores(self.dealer) > 21:
+            print('You win')
+        elif self.scores(self.dealer) < self.scores(self.player) and self.scores(self.player) < 21:
+            print('You win')
+        elif self.scores(self.dealer) == self.scores(self.player) and self.scores(self.player) < 21:
+            print('Draw')
         else:
             print('House wins!')
 
